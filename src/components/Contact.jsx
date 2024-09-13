18a72fa4ce7ba19ec1c490638a946e4402e2353c
bdiaz-dev <144321133+bdiaz-dev@users.noreply.github.com>
2024-09-13 16:16:59 +0200

    fix svg1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,11 +7,11 @@ export default function Contact ({ id = 'contact', isOnTop = false, lang }) {
       <a href={contactLinks.github} target='blank'>
         <img src={contact.gitHub} alt='githubIco' title='GitHub' />
       </a>
-      <a href={contactLinks.linkedin} target='blank'>
-        <img src={contact.linkedin} alt='linkedinIco' title='LinkedIn' />
-      </a>
       <a href={contactLinks.mail}>
         <img src={contact.mail} alt='mailIco' title='Mail' />
+      </a>
+      <a href={contactLinks.linkedin} target='blank'>
+        <img src={contact.linkedin} alt='linkedinIco' title='LinkedIn' />
       </a>
       <a href={contactLinks.cvDownload[lang === 'es' ? 0 : 1]}>
         <img src={contact.cvIco} alt='cvIcon' title='Curriculum Vitae' />
